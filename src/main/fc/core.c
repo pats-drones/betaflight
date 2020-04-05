89fdea39e9763fc96d482e96f27df45213217899
Kevin <kevindehecker@hotmail.com>
2020-04-05 11:56:26 +0200

    Disable bad rx arm safety feature, as it causes our system to never arm. Should be checked if this badrx problem is still relevant with bf4.1 and D16

--- src/main/fc/core.c
+++ src/main/fc/core.c
@@ -231,10 +231,10 @@ void updateArmingStatus(void)
 
             if (justGotRxBack && IS_RC_MODE_ACTIVE(BOXARM)) {
                 // If the RX has just started to receive a signal again and the arm switch is on, apply arming restriction
-                setArmingDisabled(ARMING_DISABLED_BAD_RX_RECOVERY);
+                //setArmingDisabled(ARMING_DISABLED_BAD_RX_RECOVERY);
             } else if (haveRx && !IS_RC_MODE_ACTIVE(BOXARM)) {
                 // If RX signal is OK and the arm switch is off, remove arming restriction
-                unsetArmingDisabled(ARMING_DISABLED_BAD_RX_RECOVERY);
+                //unsetArmingDisabled(ARMING_DISABLED_BAD_RX_RECOVERY);
             }
 
             hadRx = haveRx;
