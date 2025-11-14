71b30ecacceb5fead144f809e055f0d25be11eb3
woutervdhof <woutervandenhof@gmail.com>
2025-11-14 20:26:50 +0100

    Bitbang DShot300 on PB6 without framework

--- src/main/main.c
+++ src/main/main.c
@@ -1,116 +1,111 @@
 /*
- * This file is part of Cleanflight and Betaflight.
- *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
+ * Minimal standalone program to continuously bit-bang a DShot300 signal on PB6.
+ * Generates a constant throttle command with value 1000 (telemetry disabled).
  */
 
-#include <stdbool.h>
 #include <stdint.h>
+#include "stm32f4xx.h"
 
-#include "platform.h"
-
-#include "drivers/time.h"
-#include "drivers/motor.h"
-#include "drivers/io.h"
-#if defined(USE_TIMER_MGMT)
-#include "drivers/dma_reqmap.h"
+#ifndef DSHOT_THROTTLE_VALUE
+#define DSHOT_THROTTLE_VALUE 1000U
 #endif
 
-#include "fc/init.h"
+#ifndef DSHOT_FRAME_GAP_US
+#define DSHOT_FRAME_GAP_US 60U
+#endif
 
-#include "pg/motor.h"
-#if defined(USE_TIMER_MGMT)
-#include "pg/timerio.h"
-#endif
-#if defined(USE_I2C)
-#include "pg/bus_i2c.h"
-#endif
+static void enableDwtCycleCounter(void)
+{
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    DWT->CYCCNT = 0;
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+}
+
+static inline void delayTicks(uint32_t ticks)
+{
+    const uint32_t start = DWT->CYCCNT;
+    while ((DWT->CYCCNT - start) < ticks) {
+        __NOP();
+    }
+}
+
+static uint32_t getCoreClockHz(void)
+{
+    extern uint32_t SystemCoreClock;
+    if (SystemCoreClock != 0U) {
+        return SystemCoreClock;
+    }
+
+    return 168000000U;
+}
+
+static uint16_t buildDshotFrame(uint16_t throttle)
+{
+    throttle &= 0x7FFU;
+    uint16_t packet = (uint16_t)(throttle << 1); // Telemetry bit cleared
+
+    uint16_t checksum = 0;
+    uint16_t checksumData = packet;
+    for (int i = 0; i < 3; i++) {
+        checksum ^= (checksumData & 0xFU);
+        checksumData >>= 4;
+    }
+    checksum &= 0xFU;
+
+    packet = (uint16_t)((packet << 4) | checksum);
+    return packet;
+}
+
+static void initPb6Gpio(void)
+{
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+    (void)RCC->AHB1ENR;
+
+    GPIOB->MODER &= ~(3U << (6U * 2U));
+    GPIOB->MODER |= (1U << (6U * 2U));
+
+    GPIOB->OTYPER &= ~(1U << 6U);
+    GPIOB->OSPEEDR |= (3U << (6U * 2U));
+    GPIOB->PUPDR &= ~(3U << (6U * 2U));
+
+    GPIOB->BSRR = (uint32_t)(1U << (6U + 16U));
+}
+
+static void sendDshotFrame(uint16_t packet, uint32_t bitTicks, uint32_t oneHighTicks, uint32_t zeroHighTicks)
+{
+    for (int bit = 0; bit < 16; bit++) {
+        const uint32_t start = DWT->CYCCNT;
+        const uint32_t highTicks = (packet & 0x8000U) ? oneHighTicks : zeroHighTicks;
+
+        GPIOB->BSRR = (1U << 6U);
+        while ((DWT->CYCCNT - start) < highTicks) {
+            __NOP();
+        }
+
+        GPIOB->BSRR = (1U << (6U + 16U));
+        while ((DWT->CYCCNT - start) < bitTicks) {
+            __NOP();
+        }
+
+        packet <<= 1;
+    }
+}
 
 int main(void)
 {
-    // Configure the timer resource table so only PB6 is mapped for motor output.
-#if defined(USE_TIMER_MGMT) && (MAX_TIMER_PINMAP_COUNT > 0)
-    for (unsigned i = 0; i < MAX_TIMER_PINMAP_COUNT; i++) {
-        timerIOConfigMutable(i)->ioTag = IO_TAG_NONE;
-        timerIOConfigMutable(i)->index = 0;
-        timerIOConfigMutable(i)->dmaopt = DMA_OPT_UNUSED;
+    enableDwtCycleCounter();
+    initPb6Gpio();
+
+    const uint32_t coreClockHz = getCoreClockHz();
+    const uint32_t bitTicks = coreClockHz / 300000U;
+    const uint32_t oneHighTicks = (bitTicks * 2U) / 3U;
+    const uint32_t zeroHighTicks = bitTicks / 3U;
+    const uint32_t frameGapTicks = (uint32_t)(((uint64_t)coreClockHz * DSHOT_FRAME_GAP_US) / 1000000ULL);
+
+    const uint16_t packet = buildDshotFrame(DSHOT_THROTTLE_VALUE);
+
+    while (1) {
+        sendDshotFrame(packet, bitTicks, oneHighTicks, zeroHighTicks);
+        delayTicks(frameGapTicks);
     }
-
-    timerIOConfigMutable(0)->ioTag = IO_TAG(PB6);
-    timerIOConfigMutable(0)->index = 1;
-    timerIOConfigMutable(0)->dmaopt = DMA_OPT_UNUSED;
-#endif
-
-#if defined(USE_I2C)
-    for (int i = 0; i < I2CDEV_COUNT; i++) {
-        i2cConfigMutable(i)->ioTagScl = IO_TAG_NONE;
-        i2cConfigMutable(i)->ioTagSda = IO_TAG_NONE;
-    }
-#endif
-
-#ifdef USE_MOTOR
-    motorDevConfig_t *motorDevConfig = &motorConfigMutable()->dev;
-
-    motorDevConfig->motorPwmProtocol = PWM_TYPE_DSHOT300;
-    motorDevConfig->useBurstDshot = DSHOT_DMAR_OFF;
-    motorDevConfig->useDshotTelemetry = 0;
-    motorDevConfig->useDshotEdt = 0;
-#ifdef USE_DSHOT_BITBANG
-    motorDevConfig->useDshotBitbang = DSHOT_BITBANG_OFF;
-    motorDevConfig->useDshotBitbangedTimer = DSHOT_BITBANGED_TIMER_AUTO;
-#endif
-
-    motorDevConfig->ioTags[0] = IO_TAG(PB6);
-    for (uint8_t i = 1; i < MAX_SUPPORTED_MOTORS; i++) {
-        motorDevConfig->ioTags[i] = IO_TAG_NONE;
-    }
-    for (uint8_t i = 0; i < MAX_SUPPORTED_MOTORS; i++) {
-        motorDevConfig->motorOutputReordering[i] = i;
-    }
-#endif
-
-    init();
-
-#ifdef USE_MOTOR
-    const uint16_t dshotCommandValue = 1000;
-    const uint8_t motorCount = motorDeviceCount();
-    float motorOutputs[MAX_SUPPORTED_MOTORS];
-
-    for (uint8_t i = 0; i < MAX_SUPPORTED_MOTORS; i++) {
-        motorOutputs[i] = 0.0f;
-    }
-
-    for (uint8_t i = 0; i < motorCount && i < MAX_SUPPORTED_MOTORS; i++) {
-        const uint8_t reorderedIndex = motorConfig()->dev.motorOutputReordering[i];
-        if (reorderedIndex < MAX_SUPPORTED_MOTORS &&
-            motorConfig()->dev.ioTags[reorderedIndex] != IO_TAG_NONE) {
-            motorOutputs[i] = (float)dshotCommandValue;
-        }
-    }
-
-    while (true) {
-        motorWriteAll(motorOutputs);
-        delay(1);
-    }
-#else
-    while (true) {
-        // Nothing to do if motors are not supported on this build.
-    }
-#endif
-
-    return 0;
 }
