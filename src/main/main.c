e5122dcc2f3af28034f46e2f91ebaa01410f2fd6
woutervdhof <woutervandenhof@gmail.com>
2025-11-14 17:01:52 +0100

    Configure PB6 timer mapping before init for constant DShot output

--- src/main/main.c
+++ src/main/main.c
@@ -26,45 +26,80 @@
 #include "drivers/time.h"
 #include "drivers/motor.h"
 #include "drivers/io.h"
+#if defined(USE_TIMER_MGMT)
+#include "drivers/dma_reqmap.h"
+#endif
 
 #include "fc/init.h"
 
-#include "flight/mixer.h"
-
 #include "pg/motor.h"
+#if defined(USE_TIMER_MGMT)
+#include "pg/timerio.h"
+#endif
+#if defined(USE_I2C)
+#include "pg/bus_i2c.h"
+#endif
 
 int main(void)
 {
+    // Configure the timer resource table so only PB6 is mapped for motor output.
+#if defined(USE_TIMER_MGMT) && (MAX_TIMER_PINMAP_COUNT > 0)
+    for (unsigned i = 0; i < MAX_TIMER_PINMAP_COUNT; i++) {
+        timerIOConfigMutable(i)->ioTag = IO_TAG_NONE;
+        timerIOConfigMutable(i)->index = 0;
+        timerIOConfigMutable(i)->dmaopt = DMA_OPT_UNUSED;
+    }
+
+    timerIOConfigMutable(0)->ioTag = IO_TAG(PB6);
+    timerIOConfigMutable(0)->index = 1;
+    timerIOConfigMutable(0)->dmaopt = DMA_OPT_UNUSED;
+#endif
+
+#if defined(USE_I2C)
+    for (int i = 0; i < I2CDEV_COUNT; i++) {
+        i2cConfigMutable(i)->ioTagScl = IO_TAG_NONE;
+        i2cConfigMutable(i)->ioTagSda = IO_TAG_NONE;
+    }
+#endif
+
+#ifdef USE_MOTOR
+    motorDevConfig_t *motorDevConfig = &motorConfigMutable()->dev;
+
+    motorDevConfig->motorPwmProtocol = PWM_TYPE_DSHOT300;
+    motorDevConfig->useBurstDshot = DSHOT_DMAR_OFF;
+    motorDevConfig->useDshotTelemetry = 0;
+    motorDevConfig->useDshotEdt = 0;
+#ifdef USE_DSHOT_BITBANG
+    motorDevConfig->useDshotBitbang = DSHOT_BITBANG_OFF;
+    motorDevConfig->useDshotBitbangedTimer = DSHOT_BITBANGED_TIMER_AUTO;
+#endif
+
+    motorDevConfig->ioTags[0] = IO_TAG(PB6);
+    for (uint8_t i = 1; i < MAX_SUPPORTED_MOTORS; i++) {
+        motorDevConfig->ioTags[i] = IO_TAG_NONE;
+    }
+    for (uint8_t i = 0; i < MAX_SUPPORTED_MOTORS; i++) {
+        motorDevConfig->motorOutputReordering[i] = i;
+    }
+#endif
+
     init();
 
 #ifdef USE_MOTOR
-    motorShutdown();
-
-    motorConfigMutable()->dev.motorPwmProtocol = PWM_TYPE_DSHOT300;
-    motorConfigMutable()->dev.useDshotTelemetry = 0;
-    motorConfigMutable()->dev.useDshotEdt = 0;
-#ifdef USE_DSHOT_BITBANG
-    motorConfigMutable()->dev.useDshotBitbang = 0;
-    motorConfigMutable()->dev.useDshotBitbangedTimer = 0;
-#endif
-
-    motorConfigMutable()->dev.ioTags[0] = IO_TAG(PB6);
-    for (uint8_t i = 1; i < MAX_SUPPORTED_MOTORS; i++) {
-        motorConfigMutable()->dev.ioTags[i] = IO_TAG_NONE;
-    }
-    for (uint8_t i = 0; i < MAX_SUPPORTED_MOTORS; i++) {
-        motorConfigMutable()->dev.motorOutputReordering[i] = i;
-    }
-
-    motorDevInit(&motorConfig()->dev, motorConfig()->mincommand, 1);
-    motorEnable();
-
     const uint16_t dshotCommandValue = 1000;
     const uint8_t motorCount = motorDeviceCount();
     float motorOutputs[MAX_SUPPORTED_MOTORS];
 
+    for (uint8_t i = 0; i < MAX_SUPPORTED_MOTORS; i++) {
+        motorOutputs[i] = 0.0f;
+    }
+
     for (uint8_t i = 0; i < motorCount && i < MAX_SUPPORTED_MOTORS; i++) {
-        motorOutputs[i] = (float)dshotCommandValue;
+        const uint8_t reorderedIndex = motorConfig()->dev.motorOutputReordering[i];
+        if (reorderedIndex < MAX_SUPPORTED_MOTORS &&
+            motorConfig()->dev.ioTags[reorderedIndex] != IO_TAG_NONE) {
+            motorOutputs[i] = (float)dshotCommandValue;
+        }
     }
 
     while (true) {
